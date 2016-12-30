d0f7528fba81889b06419387811984ff0be2ee1f
System Administrator <root@Jongwonui-MacBook-Pro.local>
2016-12-31 05:31:33 +0900

    1

--- AllinOne/settings.py
+++ AllinOne/settings.py
@@ -128,10 +128,11 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [
                   os.path.join(PROJECT_ROOT, "static"),
 ]
+
+STATIC_URL = '/static/'
 
 STATICFIELS_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
