bc0baa30176a984bb800fbf251ece2337a333fc0
System Administrator <root@Jongwonui-MacBook-Pro.local>
2016-12-31 18:17:02 +0900

    css

--- AllinOne/settings.py
+++ AllinOne/settings.py
@@ -143,18 +143,13 @@ DROPBOX_SETTINGS = {
 }
 
 GOOGLEDRIVE_SETTINGS = {
-    "client_id": "1031347366738-5acih4stjqme8pnmoieg9kp2h7qpv6a5.apps.googleusercontent.com",
-    "project_id": "delta-spark-147411",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://accounts.google.com/o/oauth2/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_secret": "HRjNwv4S5hbwu4LdW99S5xQu",
-    "redirect_uri": "allinone-project.herokuapp.com/ggdrive_auth_finish",
-
-    'SCOPES': 'https://www.googleapis.com/auth/drive',
-    'CLIENT_SECRET_FILE': 'All_in_One/client_secret.json',
-    'APPLICATION_NAME': 'Drive API Python Quickstart',
-    'APPLICATION_KEY': 'AIzaSyA3TVhv1C2Am-_rpHV2rLgHrGhThcUQN14'
+    "client_id":"164066868294-6mvnhbe023t9aeqkv1j038v3vmuolqo6.apps.googleusercontent.com",
+    "project_id":"allinone-cloud",
+    "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+    "token_uri":"https://accounts.google.com/o/oauth2/token",
+    "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+    "client_secret":"1fyMojBU97jTTCvP7csMnRa3",
+    "redirect_uris":["https://allinone-project.herokuapp.com/ggdrive_auth_finish"]
 }
 
 ONEDRIVE_SETTINGS = {
