85798f0138578324fe3d57c28d2b694a15dc1c22
System Administrator <root@Jongwonui-MacBook-Pro.local>
2016-12-31 19:55:22 +0900

    dropbox id

--- All_in_one/views.py
+++ All_in_one/views.py
@@ -1598,8 +1598,10 @@ def dropbox_get_auth_flow(request):
 
     redirect_uri = request.build_absolute_uri(reverse('dropbox_auth_finish'))
     _keys = settings.DROPBOX_SETTINGS
+    for key in request.session.keys():
+        del request.session[key]
     return DropboxOAuth2Flow(
-        _keys['APP_KEY'], _keys['APP_SECRET'], redirect_uri,  'dropbox-auth-csrf-token')
+        _keys['APP_KEY'], _keys['APP_SECRET'], redirect_uri, request.session, 'dropbox-auth-csrf-token')
 
 
 
