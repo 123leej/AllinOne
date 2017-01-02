b3c1b46427791c7958522d1db10dbae592c9eac8
System Administrator <root@Jongwonui-MacBook-Pro.local>
2017-01-02 17:14:07 +0900

    1

--- AllinOne/settings.py
+++ AllinOne/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 STATICFILES_DIRS = [
-                  os.path.join(BASE_DIR, "static"),
+                  os.path.join(PROJECT_ROOT, "static"),
 ]
 
 STATIC_URL = '/static/'
